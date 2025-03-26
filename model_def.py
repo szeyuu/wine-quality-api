978c8fc7020868d47fed253abcd923671910ed70
szeyuu <166536049+szeyuu@users.noreply.github.com>
2025-03-25 23:40:41 -0400

    Add files via upload
    Update main.py and upload fixed deployment files

---
+++ model_def.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_dim):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 50)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(50, 1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
